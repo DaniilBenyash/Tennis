0f8f7fb711ff9549bc9df9daa1ec563af5a315f3
DaniilBenyash <daniilbenyash22@gmail.com>
2022-02-06 17:50:17 +0300

    tennis

--- js/script.js
+++ js/script.js
@@ -4,74 +4,122 @@ const ctx = canvas.getContext('2d');
 canvas.width = 400;
 canvas.height = 600;
 
-centerX = 200;
-centerY = 300;
+let coordX = 200;
+let coordY = 300;
 
-let radiusBall = 10;
+let speedBallY = 0;
+let speedBallX = 0;
 
-let coordBallY = 3;
-let coordBallX = 3;
+let coordPlatX = 150;
+let coordPlatY = 570;
 
-let strX = 160;
-let strY = 570;
+let coordEnemyX = 150;
+let coordEnemyY = 20;
 
-canvas.addEventListener('click',() =>{
-    strX += 10
-})
+
 addEventListener('keydown', (key) => {
     
     if(key.key == 'ArrowRight'){
-        if(strX === 320){
+        if(coordPlatX === canvas.width - 100){
             return
         }
-        strX += 10
+        coordPlatX += 10
     }
     if(key.key == 'ArrowLeft'){
-        if(strX === 0){
+        if(coordPlatX === 0){
             return
         }
-        strX -= 10
+        coordPlatX -= 10
     }
 });
+
 
 function moveBall(){
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.beginPath();
-    ctx.strokeRect(0,0,canvas.width,canvas.height)
-    
-    ctx.fillStyle = 'green';
+
+    ctx.fillStyle = '#ec6464';
    
+    ctx.fillRect(coordPlatX, coordPlatY, 100, 10)
     
-    ctx.fillRect(strX, strY, 80, 10)
+    coordY -= speedBallY
     
-    centerY -= coordBallY
-    
-    if( strX+80 > centerX && strX < centerX ){
-        if(centerY < 13) coordBallY = -coordBallY
-        if(centerY > strY-10) coordBallY = -coordBallY
-    }else{
-        if(centerY < 13) coordBallY = -coordBallY
-        
+    if(coordPlatX+100 > coordX && coordPlatX < coordX ){
+        if(coordY == coordPlatY) speedBallY = -speedBallY
     }
 
-    centerX += coordBallX
+    coordX += speedBallX
 
-    if(centerX > 390){
-        coordBallX = -coordBallX
+    if(coordX > canvas.width - 10){
+        speedBallX = -speedBallX
     } 
-    if(centerX < 10){
-        coordBallX = -coordBallX
+    if(coordX < 10){
+        speedBallX = -speedBallX
     } 
-   
-    ctx.fillStyle = 'red'
-    ctx.arc(centerX, centerY, radiusBall, 0, Math.PI * 2);
+    
+    ctx.fillStyle = '#ec6464';
+    ctx.fillRect(coordEnemyX, coordEnemyY, 100, 10)
+
+    let speedEnemy = speedBallX 
+    
+    if(coordEnemyX + 50 != coordX){
+
+        if(coordEnemyX > canvas.width - 100 || coordEnemyX < 0){
+            speedEnemy = 0
+        }
+        if(speedBallX > 0){
+            coordEnemyX += (Math.random()*speedEnemy) + 0.8
+        }
+        if(speedBallX < 0){
+            coordEnemyX += (Math.random()*speedEnemy) - 0.8
+        }
+    }
+
+    if(coordEnemyX+100 > coordX && coordEnemyX < coordX){
+        if(coordY == coordEnemyY + 10) speedBallY = -speedBallY
+    }
+
+    ctx.fillStyle = 'yellow'
+    ctx.arc(coordX, coordY, 10, 0, Math.PI * 2);
     ctx.fill()
 
-    if( centerY > 590){
-        alert('sasai kudasai')
-        cancelAnimationFrame()
+    if(coordY < 10){
+        document.querySelector('.valYou').innerText++
+        cancelAnimationFrame(moveBall);
+        return
+    }
+    if(coordY > 590){
+        document.querySelector('.valEnemy').innerText++
+        cancelAnimationFrame(moveBall);
+        return
     };
-    requestAnimationFrame(moveBall);
+    requestAnimationFrame(moveBall)
 }
 moveBall();
 
+function start(){
+    coordX = 200;
+    coordY = 300;
+    coordPlatX = 150;
+    coordPlatY = 570;
+    coordEnemyX = 150;
+    coordEnemyY = 20;
+    moveBall();
+}
+
+document.querySelector('.butPlay').addEventListener('click',() => {
+    if(speedBallX == 0){
+        speedBallX = 3;
+        speedBallY = 3
+    }
+    if(coordY < 10){
+        speedBallX = -3;
+        speedBallY = -3
+        start();
+    }
+    if(coordY > 590){
+        speedBallX = 3;
+        speedBallY = 3
+        start();
+    }
+})
